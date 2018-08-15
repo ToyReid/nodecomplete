d1718e2f75816387b27fa44200ccfba46e88c0ba
Toy Reid <toymreid@gmail.com>
2018-08-14 21:35:36 -0400

    Lecture 21

--- notes-node/notes.js
+++ notes-node/notes.js
@@ -2,17 +2,25 @@ console.log('Starting notes.js');
 
 const fs = require('fs');
 
+var fetchNotes = () => {
+	try {
+		var notesString = fs.readFileSync('notes-data.json');
+		return JSON.parse(notesString);
+	} catch (error) {
+		return [];
+	}
+}
+
+var saveNotes = (notes) => {
+	fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+}
+
 var addNote = (title, body) => {
-	var notes = [];
+	var notes = fetchNotes();
 	var note = {
 		title,
 		body
-	};
-
-	try {
-		var notesString = fs.readFileSync('notes-data.json');
-		notes = JSON.parse(notesString);
-	} catch (error) {};
+	}
 
 	// Enumerate through notes and determine if there is already a note with given title
 	var duplicateNotes = notes.filter((note) => note.title === title);
@@ -22,25 +30,38 @@ var addNote = (title, body) => {
 
 	if (duplicateNotes.length === 0) {
 		notes.push(note);
-		fs.writeFileSync('notes-data.json', JSON.stringify(notes));
-	};
-};
+		saveNotes(notes);
+		return note;
+	}
+}
 
 var getAll = () => {
 	console.log('Getting all notes');
-};
+}
 
 var getNote = (title) => {
-	console.log('Getting note', title);
-};
+	var notes = fetchNotes();
+	return (notes.filter((note) => note.title === title))[0];
+}
 
 var removeNote = (title) => {
-	console.log('Removing note', title);
-};
+	var notes = fetchNotes();
+	var filteredNotes = notes.filter((note) => note.title !== title);
+	saveNotes(filteredNotes);
+
+	return notes.length !== filteredNotes.length;
+}
+
+var logNote = (note) => {
+	console.log('--');
+	console.log(`Title: ${note.title}`);
+	console.log(`Body: ${note.body}`);
+}
 
 module.exports = {
 	addNote,
 	getAll,
 	getNote,
-	removeNote
-};
+	removeNote,
+	logNote
+}
---
+++ notes-node/playground/debugging.js
@@ -0,0 +1,13 @@
+var person = {
+	name: 'Toy'
+};
+
+person.age = 23;
+person.age = 24;
+
+// Break point
+debugger;
+
+person.name = 'Mike';
+
+console.log(person);
